32947729df31e184197d903770c5b5afeb4dd22c
Filipe Fernandes <filipeferibeiro@gmail.com>
2026-02-19 21:40:57 -0300

    Initial Commit

---
+++ FinFlow/FinFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinFlowApp.swift
+//  FinFlow
+//
+//  Created by Filipe Fernandes on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FinFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
